3b19bb1929738ecef2b591781146eb298aac60c8
William <wtdmwtdmwtdm@gmail.com>
2026-05-24 19:59:37 -0400

    working wisp server

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const { server } = require("wisp-server-node");
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Wisp server running");
+});
+
+app.on("upgrade", (req, socket, head) => {
+  server.handleUpgrade(req, socket, head);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Listening on", PORT);
+});
